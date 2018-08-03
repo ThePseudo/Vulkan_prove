06ced3e40f2f4cc40903c92d83c33607b2c56f2e
Pseudo <andrea1995.c@live.com>
2018-08-03 11:56:26 +0200

    Documented ShaderFactory

--- Vulkan_prove/ShaderFactory.h
+++ Vulkan_prove/ShaderFactory.h
@@ -6,6 +6,12 @@
 
 using namespace std;
 
+/*
+Each shader module assumes there is the "main" function as starter
+No constants used for shaders.
+If a custom and more appropriate shader is needed, the user should create it himself,
+using the "createBasicShader" function as a pattern to simplify its initialization
+*/
 class ShaderFactory
 {
 	enum shader_type {
@@ -25,7 +31,9 @@ public:
 	//Shader destructor
 	static void destroyShader(vk::Device &device, vk::ShaderModule &shader);
 private:
+
 	static vk::ShaderModule createBasicShader(vk::Device &device, shader_type type, string inputFile);
+
 	static vector<char> readShader(string inputFile); // The file is supposed to be SPIR-V compiled
 };
 
